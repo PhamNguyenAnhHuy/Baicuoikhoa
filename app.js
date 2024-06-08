91be89a310aaa4b105e41d9127ab95b59ca5c0f9
PhamNguyenAnhHuy <phamanhhuy2k8@gmailcom>
2024-06-08 09:39:13 +0700

    done

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ var passwordInput = document.getElementById('pwdId');
 var signUpBtn = document.getElementById('SignupBTN');
 var cPassword = document.getElementById('cpassword');
 
-function SignUp(){
+function SignUp() {
     var username = unameInput.value;
     var password = passwordInput.value;
     var cPass = cPassword.value;
 
-    if( username == "" || password == ""){
+    if (username == "" || password == "") {
         alert("Try again");
     }
-    else if(password == cPass){
+    else if (password == cPass) {
         var newObj = {
             name: username,
             password: password,
         }
         var newJSON = JSON.stringify(newObj);
-        localStorage.setItem("tk_"+username, newJSON);
+        localStorage.setItem("tk_" + username, newJSON);
         alert("dang ky thanh cong");
-    window.location.href = "login.html";
+        window.location.href = "login.html";
 
     }
-    else{
+    else {
         alert("dang ky that bai");
 
     }
@@ -32,24 +32,24 @@ function SignUp(){
 function SignIn() {
     var ndTKLogin = document.getElementById("unameId").value;
     var ndMkLogin = document.getElementById("pwdId").value;
-    var checkJSON = localStorage.getItem("tk_"+ndTKLogin);
+    var checkJSON = localStorage.getItem("tk_" + ndTKLogin);
 
     console.log(ndTKLogin)
-    if(checkJSON != null){
+    if (checkJSON != null) {
         var checkObj = JSON.parse(checkJSON);
-        if(ndMkLogin == checkObj.password){
+        if (ndMkLogin == checkObj.password) {
             alert("Congratulation!");
-        window.location = "./Begin.html";
+            window.location = "./Begin.html";
         }
-        else{
+        else {
             alert("Sign in fail!")
         }
-    }else{
+    } else {
         alert("Account not found!!!")
     }
 }
 
-function SignOut(){
+function SignOut() {
     window.location.href = "login.html"
 }
 
@@ -58,27 +58,27 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 
@@ -92,16 +92,46 @@ const sunset = document.querySelector(".sunset");
 const humidity = document.querySelector(".humidity");
 const windSpeed = document.querySelector(".wind-speed");
 
-const API_KEY ="e5f09c8f6b6b9f0bb90fd299f434ecef";
+const API_KEY = "e5f09c8f6b6b9f0bb90fd299f434ecef";
 const DEFAULT_VALUE = "--";
 
-searchInput.addEventListener("change", (event) =>{
+searchInput.addEventListener("change", (event) => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&APPID=${API_KEY}&units=metric&lang=Vi`)
-    .then(response => response.json())
-    .then((data)=>{
-        cityName.innerHTML = data.name || DEFAULT_VALUE;
-        weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
-        temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
-        console.log(data);
-    })
+        .then(response => response.json())
+        .then((data) => {
+            cityName.innerHTML = data.name || DEFAULT_VALUE;
+            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+            console.log(data);
+        })
 });
+
+
+
+function sendFeedback() {
+    var name = document.getElementById('fb_name').value;
+    var email = document.getElementById('fb_email').value;
+    // var dateTime = document.getElementById('fb_dateTime');
+    var message = document.getElementById('fb_message').value;
+
+
+    if( name == "" || email == "" || message == "" ){
+        alert("Try again");
+    }
+    else{
+        var newObj = {
+            name: name,
+            email: email,
+            message: message,
+            // dateTime: dateTime,
+        }
+        var newJSON = JSON.stringify(newObj);
+        localStorage.setItem("fb_"+name, newJSON);
+        alert("Da gui");
+
+
+    }
+
+    console.log(message);
+
+}
